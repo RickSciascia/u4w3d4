eea3ffb82ba336c43d49cbac91e08aca6c6d9aa9
Riccardo <richi-97@live.it>
2026-01-22 15:07:28 +0100

    setup

---
+++ src/main/java/ricksciascia/entities/Stato.java
@@ -0,0 +1,5 @@
+package ricksciascia.entities;
+
+public enum Stato {
+	CONFERMATA, DA_CONFERMARE
+}
---
+++ src/main/java/ricksciascia/entities/TipoEvento.java
@@ -0,0 +1,5 @@
+package ricksciascia.entities;
+
+public enum TipoEvento {
+    PUBBLICO, PRIVATO
+}
